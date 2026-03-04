41dd3dffa6113d314bac9899250b344d7ded6dff
Gabriela Grabarska <gabriela.grabarska@studenci.collegiumwitelona.pl>
2026-03-04 13:34:28 +0100

    init

---
+++ src/main/java/com/pokiepaws/api/PokiepawsApiApplication.java
@@ -0,0 +1,13 @@
+package com.pokiepaws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokiepawsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokiepawsApiApplication.class, args);
+	}
+
+}
