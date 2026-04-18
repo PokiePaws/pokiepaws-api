07dd0b732c0c2161aedf9dc678849cdfbbe6c0e7
Gabriela <gabriela.grabarska@studenci.collegiumwitelona.pl>
2026-04-18 21:45:45 +0200

    fix

--- src/test/java/com/pokiepaws/api/integration/BaseIntegrationTest.java
+++ src/test/java/com/pokiepaws/api/integration/BaseIntegrationTest.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -15,10 +16,14 @@ public abstract class BaseIntegrationTest {
 
   @Container
   static final PostgreSQLContainer<?> POSTGRES =
-      new PostgreSQLContainer<>("postgres:16-alpine")
-          .withDatabaseName("pokiepaws_test")
-          .withUsername("test_user")
-          .withPassword("test_password");
+          new PostgreSQLContainer<>("postgres:16-alpine")
+                  .withDatabaseName("pokiepaws_test")
+                  .withUsername("test_user")
+                  .withPassword("test_password");
+  @Container
+  static final GenericContainer<?> MAILPIT =
+          new GenericContainer<>("axllent/mailpit:latest")
+                  .withExposedPorts(1025, 8025);
 
   @DynamicPropertySource
   static void registerProps(DynamicPropertyRegistry registry) {
@@ -26,5 +31,12 @@ public abstract class BaseIntegrationTest {
     registry.add("spring.datasource.username", POSTGRES::getUsername);
     registry.add("spring.datasource.password", POSTGRES::getPassword);
     registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+
+    registry.add("spring.mail.host", MAILPIT::getHost);
+    registry.add("spring.mail.port", () -> MAILPIT.getMappedPort(1025));
+
+    registry.add("spring.mail.properties.mail.smtp.auth", () -> "false");
+    registry.add("spring.mail.properties.mail.smtp.starttls.enable", () -> "false");
+    registry.add("spring.mail.properties.mail.smtp.starttls.required", () -> "false");
   }
 }
